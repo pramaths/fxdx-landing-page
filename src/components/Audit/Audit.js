3d715699d998d245e3c33fa65fed3b86bfd3204f
pramaths <103276292+pramaths@users.noreply.github.com>
2023-08-06 19:36:18 +0530

    Update Audit.js

--- src/components/Audit/Audit.js
+++ src/components/Audit/Audit.js
@@ -7,9 +7,9 @@ const Audit = () => {
     <div className="Audit-section">
       
           <h1 className="Audit-heading">Security First</h1>
-          <div className="subheading-container">
+         
             <img className="audit-image" src={Auditimage} alt="Audit terminal" />
-            <br/>
+             <div className="subheading-container">
             <p className="Audit-text">
             <span className="paragraph-start">  Our smart contracts have undergone rigorous unit testing and have been subjected to several independent audits conducted by <strong>“The ARCADIA GROUP”.</strong> </span></p>
 
